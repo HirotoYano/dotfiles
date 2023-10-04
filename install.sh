335d7f3c8e9f6995659abfaa892e115257c72de6
HirotoYano <mitune@mc2wifi067053.it.osakac.ac.jp>
2023-10-04 18:33:00 +0900

    Corrected process.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-target_directory="."
+target_directory="$HOME/dotfiles"
 link_directory="$HOME/.config/nvim"
 
 if [ -d "$link_directory" ]; then
@@ -8,20 +8,20 @@ if [ -d "$link_directory" ]; then
 else
     mkdir -p $link_directory
     echo "Created $directory_to_check"
-    ln -s "$target_directory/nvim/init.lua" "$link_directory/nvim/init.lua"
+    ln -s "$target_directory/nvim/init.lua" "$link_directory/init.lua"
 fi
 
 
 if [ -d "$link_directory/lua" ]; then
     echo "The directory $directory_to_check exists."
 else
-    mkdir -p $link_directory
+    mkdir -p "$link_directory/lua"
     echo "Created $directory_to_check"
-    ln -s "$target_directory/nvim/lua/base.lua" "$link_directory/nvim/lua/base.lua"
-    ln -s "$target_directory/nvim/lua/keymap.lua" "$link_directory/nvim/lua/keymap.lua"
-    ln -s "$target_directory/nvim/lua/lsp_config.lua" "$link_directory/nvim/lua/lsp_config.lua"
-    ln -s "$target_directory/nvim/lua/nvim_cmp_config.lua" "$link_directory/nvim/lua/nvim_cmp_config.lua"
-    ln -s "$target_directory/nvim/lua/plugins.lua" "$link_directory/nvim/lua/plugins.lua"
+    ln -s "$target_directory/nvim/lua/base.lua" "$link_directory/lua/base.lua"
+    ln -s "$target_directory/nvim/lua/keymap.lua" "$link_directory/lua/keymap.lua"
+    ln -s "$target_directory/nvim/lua/lsp_config.lua" "$link_directory/lua/lsp_config.lua"
+    ln -s "$target_directory/nvim/lua/nvim_cmp_config.lua" "$link_directory/lua/nvim_cmp_config.lua"
+    ln -s "$target_directory/nvim/lua/plugins.lua" "$link_directory/lua/plugins.lua"
 fi
 
 echo "Symlink creation complete."
